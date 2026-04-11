711c91819405660c8366b01d9ec4240d21055f64
Theosdoor <theo.farrell99@outlook.com>
2026-04-11 15:16:57 +0100

    add astro sitmap and clean up duplicated css

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,18 @@
 import { defineConfig } from 'astro/config';
 import yaml from '@rollup/plugin-yaml';
 
+import sitemap from '@astrojs/sitemap';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://theosdoor.github.io',
   output: 'static',
+
   vite: {
-    plugins: [yaml()],
+    plugins: [yaml(), tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
